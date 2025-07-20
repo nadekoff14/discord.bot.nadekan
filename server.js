a83855385be52f9dc79ff672d0ac9543df7264cf
nadekoff14 <nadekoff14@icloud.com>
2025-07-21 03:24:31 +0900

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ client.on("ready", (message) => {
         client.user.setActivity({ 
             name: 'BOTテスト',
             type: ActivityType.Competing
-        });
+        );
 
     
 //ping表示
