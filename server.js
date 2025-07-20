7ed58dabc12f7b0828d2acda873e43dba00af4f3
nadekoff14 <nadekoff14@icloud.com>
2025-07-21 05:52:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ http
 //BOTステータス
 client.on("ready", (message) => {
   console.log("Bot準備完了～");
-  client.user.setActivity("人類を選別", { type: 'COMPETING' });
+  client.user.setActivity("人類を選別");
     });
 
 
